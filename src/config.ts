c5605f99147cb4ca155bf4ef6c98897085ab3c5e
Richard Liu <richard.y.liu@berkeley.edu>
2023-11-13 19:16:13 +0300

    make generic for any validEventIds

--- src/config.ts
+++ src/config.ts
@@ -1,66 +1,7 @@
-export const ZUPASS_URL = "https://zupass.org"
+export const ZUPASS_URL = "https://zupass.org";
 
 // https://api.zupass.org/issue/eddsa-public-key
 export const zupassPublicKey: [string, string] = [
-    "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
-    "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f"
-]
-
-const ticketTypeNames = ["ZuzaluResident", "ZuzaluOrganizer", "ZuzaluVisitor", "ZuConnectResident"] as const
-
-type TicketTypeName = (typeof ticketTypeNames)[number]
-
-/**
- * We want to match a ticket based on a pairing of event IDs and product IDs.
- * We also want to divide these into categories or "types" of ticket. There
- * are four types, as defined above, and each type has one or more pairs of
- * event and product IDs that qualify a ticket as belonging to that group.
- *
- * With this data, we can classify a user's ticket and use this to make some
- * decisions about what access to grant, or other features to enable or
- * disable.
- */
-const ticketTypes: Record<TicketTypeName, Array<{ eventId: string; productId: string }>> = {
-    ZuzaluResident: [
-        {
-            eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
-            productId: "5ba4cd9e-893c-4a4a-b15b-cf36ceda1938"
-        }
-    ],
-    ZuzaluOrganizer: [
-        {
-            eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
-            productId: "10016d35-40df-4033-a171-7d661ebaccaa"
-        }
-    ],
-    ZuzaluVisitor: [
-        {
-            eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
-            productId: "53b518ed-e427-4a23-bf36-a6e1e2764256"
-        }
-    ],
-    ZuConnectResident: [
-        {
-            eventId: "91312aa1-5f74-4264-bdeb-f4a3ddb8670c",
-            productId: "cc9e3650-c29b-4629-b275-6b34fc70b2f9"
-        },
-        {
-            eventId: "54863995-10c4-46e4-9342-75e48b68d307",
-            productId: "d2123bf9-c027-4851-b52c-d8b73fc3f5af"
-        },
-        {
-            eventId: "797de414-2aec-4ef8-8655-09df7e2b6cc6",
-            productId: "d3620f38-56a9-4235-bea8-0d1dba6bb623"
-        },
-        {
-            eventId: "a6109324-7ca0-4198-9583-77962d1b9d53",
-            productId: "a6109324-7ca0-4198-9583-77962d1b9d53"
-        }
-    ]
-}
-
-// Map the above data structure into a simple array of event IDs.
-export const supportedEvents = Object.values(ticketTypes).flatMap((items) => items.map((item) => item.eventId))
-
-// Map the above data structure into a simple array of product IDs.
-export const supportedProducs = Object.values(ticketTypes).flatMap((items) => items.map((item) => item.productId))
+  "05e0c4e8517758da3a26c80310ff2fe65b9f85d89dfc9c80e6d0b6477f88173e",
+  "29ae64b615383a0ebb1bc37b3a642d82d37545f0f5b1444330300e4c4eedba3f",
+];
--- src/utils.ts
+++ src/utils.ts
@@ -1,92 +1,89 @@
-import { EdDSATicketPCDPackage } from "@pcd/eddsa-ticket-pcd"
-import { ArgumentTypeName } from "@pcd/pcd-types"
-import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd"
+import { EdDSATicketPCDPackage } from "@pcd/eddsa-ticket-pcd";
+import { ArgumentTypeName } from "@pcd/pcd-types";
+import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
 import {
-    EdDSATicketFieldsToReveal,
-    ZKEdDSAEventTicketPCDArgs,
-    ZKEdDSAEventTicketPCDPackage
-} from "@pcd/zk-eddsa-event-ticket-pcd"
-import { constructZupassPcdGetRequestUrl } from "./passport-interface/PassportInterface"
-import { ZUPASS_URL, supportedEvents, supportedProducs, zupassPublicKey } from "./config"
-import { openZupassPopup } from "./passport-interface/PassportPopup"
+  EdDSATicketFieldsToReveal,
+  ZKEdDSAEventTicketPCDArgs,
+  ZKEdDSAEventTicketPCDPackage,
+} from "@pcd/zk-eddsa-event-ticket-pcd";
+import { constructZupassPcdGetRequestUrl } from "./passport-interface/PassportInterface";
+import { ZUPASS_URL, zupassPublicKey } from "./config";
+import { openZupassPopup } from "./passport-interface/PassportPopup";
 
 /**
  * Opens a Zupass popup to make a proof of a ZK EdDSA event ticket PCD.
  */
 export function openZKEdDSAEventTicketPopup(
-    fieldsToReveal: EdDSATicketFieldsToReveal,
-    watermark: string | bigint,
-    validEventIds: string[] = supportedEvents,
-    validProductIds: string[] = supportedProducs,
-    popupRoute: string = "popup"
+  fieldsToReveal: EdDSATicketFieldsToReveal,
+  watermark: string | bigint,
+  validEventIds: string[] | undefined,
+  popupRoute: string = "popup"
 ) {
-    const args: ZKEdDSAEventTicketPCDArgs = {
-        ticket: {
-            argumentType: ArgumentTypeName.PCD,
-            pcdType: EdDSATicketPCDPackage.name,
-            value: undefined,
-            userProvided: true,
-            validatorParams: {
-                eventIds: validEventIds,
-                productIds: validProductIds,
-                notFoundMessage: "No eligible PCDs found"
-            }
-        },
-        identity: {
-            argumentType: ArgumentTypeName.PCD,
-            pcdType: SemaphoreIdentityPCDPackage.name,
-            value: undefined,
-            userProvided: true
-        },
-        validEventIds: {
-            argumentType: ArgumentTypeName.StringArray,
-            value: validEventIds.length != 0 ? validEventIds : undefined,
-            userProvided: false
-        },
-        fieldsToReveal: {
-            argumentType: ArgumentTypeName.ToggleList,
-            value: fieldsToReveal,
-            userProvided: false
-        },
-        watermark: {
-            argumentType: ArgumentTypeName.BigInt,
-            value: watermark.toString(),
-            userProvided: false
-        },
-        externalNullifier: {
-            argumentType: ArgumentTypeName.BigInt,
-            value: watermark.toString(),
-            userProvided: false
-        }
-    }
+  const args: ZKEdDSAEventTicketPCDArgs = {
+    ticket: {
+      argumentType: ArgumentTypeName.PCD,
+      pcdType: EdDSATicketPCDPackage.name,
+      value: undefined,
+      userProvided: true,
+      validatorParams: {
+        eventIds: [],
+        productIds: [],
+        notFoundMessage: "No eligible PCDs found",
+      },
+    },
+    identity: {
+      argumentType: ArgumentTypeName.PCD,
+      pcdType: SemaphoreIdentityPCDPackage.name,
+      value: undefined,
+      userProvided: true,
+    },
+    validEventIds: {
+      argumentType: ArgumentTypeName.StringArray,
+      value: validEventIds?.length != 0 ? validEventIds : undefined,
+      userProvided: false,
+    },
+    fieldsToReveal: {
+      argumentType: ArgumentTypeName.ToggleList,
+      value: fieldsToReveal,
+      userProvided: false,
+    },
+    watermark: {
+      argumentType: ArgumentTypeName.BigInt,
+      value: watermark.toString(),
+      userProvided: false,
+    },
+    externalNullifier: {
+      argumentType: ArgumentTypeName.BigInt,
+      value: watermark.toString(),
+      userProvided: false,
+    },
+  };
 
-    const popupUrl = window.location.origin + "/" + popupRoute
+  const popupUrl = window.location.origin + "/" + popupRoute;
 
-    const proofUrl = constructZupassPcdGetRequestUrl<typeof ZKEdDSAEventTicketPCDPackage>(
-        ZUPASS_URL,
-        popupUrl,
-        ZKEdDSAEventTicketPCDPackage.name,
-        args,
-        {
-            genericProveScreen: true,
-            title: "ZKEdDSA Ticket Proof",
-            description: "ZKEdDSA Ticket PCD Request"
-        }
-    )
+  const proofUrl = constructZupassPcdGetRequestUrl<
+    typeof ZKEdDSAEventTicketPCDPackage
+  >(ZUPASS_URL, popupUrl, ZKEdDSAEventTicketPCDPackage.name, args, {
+    genericProveScreen: true,
+    title: "ZKEdDSA Ticket Proof",
+    description: "ZKEdDSA Ticket PCD Request",
+  });
 
-    openZupassPopup(popupUrl, proofUrl)
+  openZupassPopup(popupUrl, proofUrl);
 }
 
 export function parseSerializedPCD(serializedPCD: string): string | undefined {
-    try {
-        const { pcd } = JSON.parse(serializedPCD)
+  try {
+    const { pcd } = JSON.parse(serializedPCD);
 
-        return pcd
-    } catch {
-        return undefined
-    }
+    return pcd;
+  } catch {
+    return undefined;
+  }
 }
 
 export function isZupassPublicKey(publicKey: [string, string]): boolean {
-    return zupassPublicKey[0] === publicKey[0] && zupassPublicKey[1] === publicKey[1]
+  return (
+    zupassPublicKey[0] === publicKey[0] && zupassPublicKey[1] === publicKey[1]
+  );
 }
